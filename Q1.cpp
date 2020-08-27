52fc283901d29177c1cbd7087a65975c294088d5
Mohammad Walid Shaarawi <mwalidshaarawi@users.noreply.github.com>
2020-08-28 02:36:09 +0300

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+
+
+    cout << " *********             ***                     *                    *     " <<endl ;
+    cout << " *       *            *   *                   ***                  * *    " <<endl ;
+    cout << " *       *          *       *                *****                *   *   " <<endl ;
+    cout << " *       *        *           *                *                 *     *  " <<endl ;
+    cout << " *       *       *             *               *                *       * " <<endl ;
+    cout << " *       *       *             *               *               *         *" <<endl ;
+    cout << " *       *       *             *               *                *       * " <<endl ;
+    cout << " *       *        *           *                *                 *     *  " <<endl ;
+    cout << " *       *          *       *                  *                  *   *   " <<endl ;
+    cout << " *       *            *   *                    *                   * *    " <<endl ;
+    cout << " *********             ***                     *                    *     " <<endl ;
+
+
+
+    cout << "  SQUARE              OVAL                   ARROW              DIAMOND  "  <<endl ;
+
+
+return 0;
+
+
+
+
+}
